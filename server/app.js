d94bfc64287a283ac32a86b209a62038f10edd06
Gaurav775-Git <gaurav775.official@gmail.com>
2026-07-20 12:36:11 +0530

    tried to add the db connection but falied

--- server/app.js
+++ server/app.js
@@ -1,12 +1,23 @@
+require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var pool = require('./config/db');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Database connection failed:', err.message);
+  } else {
+    console.log('Database connected successfully!');
+  }
+});
 
 app.use(logger('dev'));
 app.use(express.json());
