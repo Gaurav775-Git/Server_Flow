5a08a549f0b1191ff22e3f8ab3ed9e5fedf52845
Gaurav775-Git <gaurav775.official@gmail.com>
2026-07-23 10:46:01 +0530

    server updated with db connection this time solid db connected

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var { query } = require('./config/db');  //   Correct import
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,20 +23,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//   FIXED: Test route
+app.get('/test', async (req, res) => {
+    try {
+        const result = await query('SELECT NOW() as time');
+        res.json({  //   Changed from req.json to res.json
+            success: true,
+            message: '  Database connected!',
+            time: result.rows[0].time  //   Changed from result.row to result.rows
+        });
+    } catch (err) {  //   Changed from 'error' to 'err'
+        console.error('Query error:', err);
+        res.status(500).json({
+            success: false,
+            error: err.message  //   Changed from error.message to err.message
+        });
+    }
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
