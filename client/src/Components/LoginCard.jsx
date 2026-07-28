178462b085b892d70aa269a34d3099fe63093e1b
Harsh Agarwal <harshagarwal31.creator@gmail.com>
2026-07-28 15:01:39 +0530

    tailwindcss is setup & auth structure created

---
+++ client/src/Components/LoginCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginCard = () => {
+  return <div className="w-full max-w-[440px] h-[600px] bg-[#141920]"></div>;
+};
+
+export default LoginCard;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginCard from "../Components/LoginCard";
+
+const Login = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-[#10131A]">
+      <LoginCard />
+    </div>
+  );
+};
+
+export default Login;
