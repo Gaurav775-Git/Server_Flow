dcfeeeaf04def75ddc1b0186179978d779aac741
Harsh Agarwal <harshagarwal31.creator@gmail.com>
2026-07-29 11:38:56 +0530

    login card created

--- client/src/Components/LoginCard.jsx
+++ client/src/Components/LoginCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LoginCard = () => {
-  return <div className="w-full max-w-[440px] h-[600px] bg-[#141920]"></div>;
+  return <div className="w-full max-w-110 h-150 bg-[#141920]"></div>;
 };
 
 export default LoginCard;
