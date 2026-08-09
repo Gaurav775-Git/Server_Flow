a5a407d0c26a3023e85e396f8346830679868923
Gaurav775-Git <gaurav775.official@gmail.com>
2026-08-09 19:09:17 +0530

    NavBar.jsx and Footer.jsx Created with Home page architecture proper alingment

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import NavBar from '../components/NavBar';
+
+const Home = () => {
+  return (
+    <div className="bg-[#0b0e14] min-h-screen">
+      <NavBar />
+      {/* Rest of your content */}
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,6 @@
-import React from "react";
-import LoginCard from "../Components/LoginCard";
-import LoginLeftIcon from "../Components/ui/LoginLeftIcon";
-import LoginRightIcon from "../Components/ui/LoginRightIcon";
+import LoginCard from "../components/LoginCard";
+import LoginLeftIcon from "../components/ui/LoginLeftIcon";
+import LoginRightIcon from "../components/ui/LoginRightIcon";
 const Login = () => {
   return (
     <div className="min-h-screen relative flex justify-center items-center bg-[#10131A]">
