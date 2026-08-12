60589f352d09c11dc84a97b5d7895f39f50fcf57
Gaurav Goswami <gaurav775.official@gmail.com>
2026-08-12 08:38:53 +0530

    Make it commented until the testing and running is done so that production does not effcted.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,14 +5,14 @@ import Canvas from "./components/sections/dashboard/Canvas";
 import Sidebar from "./components/sections/dashboard/Sidebar";
 
 const App = () => {
-  // return window.location.pathname === "/dashboard" ? <DashboardPage /> : <Home />;
-  return (
-    <div className="grid grid-cols-[340px_1fr] grid-rows-[1fr] h-screen">
-      <Sidebar />
-      <main className="min-w-0 h-full">
-        <Canvas />
-      </main>
-    </div>
+  return window.location.pathname === "/dashboard" ? <DashboardPage /> : <Home />;
+  // return (
+  //   <div className="grid grid-cols-[340px_1fr] grid-rows-[1fr] h-screen">
+  //     <Sidebar />
+  //     <main className="min-w-0 h-full">
+  //       <Canvas />
+  //     </main>
+  //   </div>
   );
 };
 
