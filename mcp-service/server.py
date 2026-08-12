95d0d2f876a7e36b70759981d2a714e558d151a0
Gaurav775-Git <gaurav775.official@gmail.com>
2026-08-13 00:16:57 +0530

    Added read file tool
    mcp tool is adden to the server with the read file capability using os module of python

--- mcp-service/server.py
+++ mcp-service/server.py
@@ -1,10 +1,19 @@
+import os
 from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP("Server_Flow")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 @mcp.tool()
 def hello(name:str)->str:
     print("hello , {name}")
+
+@mcp.tool()
+def read_file(filename:str)->str:
+    path = os.path.join(BASE_DIR,os.path.basename(filename))
+    with open (path ,"r") as f:
+        return f.read()
+
 
 if __name__ == "__main__":
     print("server is running")
