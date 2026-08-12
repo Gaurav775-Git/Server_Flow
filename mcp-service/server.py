877f8d1e54eb34ae1e25c0023de2f515dce05dcd
Gaurav775-Git <gaurav775.official@gmail.com>
2026-08-13 01:04:49 +0530

    File create tool
    created a file create tool on the server.py on the mcp service side

--- mcp-service/server.py
+++ mcp-service/server.py
@@ -12,7 +12,7 @@ def hello(name: str) -> str:
 @mcp.tool()
 def read_file(filename: str) -> str:
     """Read and return the contents of a text file in the server's directory."""
-    print("read file called")
+    # print("read file called")
     path = os.path.join(BASE_DIR, os.path.basename(filename))
     with open(path, "r") as f:
         return f.read()
@@ -20,11 +20,26 @@ def read_file(filename: str) -> str:
 @mcp.tool()
 def write_file(filename: str, content: str) -> str:
     """Write text content to a file in the server's directory. Overwrites if it already exists."""
-    print("write tool is called.")
+    # print("write tool is called.")
     path = os.path.join(BASE_DIR, os.path.basename(filename))
     with open(path, "w") as f:
         f.write(content)
     return f"data inserted in {filename} successfully"
+
+@mcp.tool()
+def create_file(filename , content: str)->str:
+    """Create a file if not exist and write content which the user ask for ."""
+    path = os.path.join(BASE_DIR , os.path.basename(filename))
+
+    if os.path.exists(path):
+        return f"File exists {filename} in this directory."
+
+    with open(path , "w") as f:
+        f.write(content)
+
+    return f"file is created with name {filename}"    
+    
+
 
 if __name__ == "__main__":
     mcp.run(transport="stdio")
