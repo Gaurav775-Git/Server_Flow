44cdf8c6acafcda41e110a14e7f5884bd373925c
gauravsri26 <srivastavagaurav2605@gmail.com>
2026-08-12 21:17:14 +0530

    feature: added products section to landing page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Footer from '../components/Footer';
 import NavBar from '../components/NavBar';
 import HeroSection from '../components/sections/home/HeroSection';
+import ProductsSection from "../components/sections/home/ProductsSection";
 
 const Home = () => {
   return (
     <div className="bg-[#05090E] min-h-screen">
       <NavBar />
       <HeroSection/>
+       <ProductsSection />
       <Footer />
     </div>
   );
