68c31654338d65493ddfbbef443cf313021b9b9c
Gaurav775-Git <gaurav775.official@gmail.com>
2026-08-14 02:05:18 +0530

    React-router-dom added
    Things are working now properl using react route dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,16 @@ import "./index.css";
 import Canvas from "./components/sections/dashboard/Canvas";
 import Sidebar from "./components/sections/dashboard/Sidebar";
 
+import {BrowserRouter as Router , Routes ,Route} from 'react-router-dom'
+
 const App = () => {
-  return window.location.pathname === "/dashboard" ? <DashboardPage /> : <Home />;
-  // return (
-  //   <div className="grid grid-cols-[340px_1fr] grid-rows-[1fr] h-screen">
-  //     <Sidebar />
-  //     <main className="min-w-0 h-full">
-  //       <Canvas />
-  //     </main>
-  //   </div>
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/dashboard" element={<DashboardPage/>}/>
+      </Routes>
+    </Router>
   );
 };
 
