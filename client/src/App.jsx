a1181ecb858c8a1b7312ffb3638f5e96516b5298
Gaurav775-Git <gaurav775.official@gmail.com>
2026-08-14 02:32:08 +0530

    Playground page work initated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Canvas from "./components/sections/dashboard/Canvas";
 import Sidebar from "./components/sections/dashboard/Sidebar";
 
 import {BrowserRouter as Router , Routes ,Route} from 'react-router-dom'
+import Playground_Page from "./pages/Playground_Page";
 
 const App = () => {
   return(
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/dashboard" element={<DashboardPage/>}/>
+        <Route path="/playground" element={<Playground_Page/>}/>
       </Routes>
     </Router>
   );
