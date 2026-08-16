ea2a6154bea66208accd6a3b97f651c980b14247
Gaurav Goswami <gaurav775.official@gmail.com>
2026-08-16 21:03:08 +0530

    Revert "feat: implement profile update and password change APIs"

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -8,14 +8,3 @@
 // // router.get('/add', adduser);
 
 // // module.exports = router;
-const express = require("express");
-const { updateProfile, updatePassword } = require("../controllers/userController");
-const { verifyToken } = require("../middlewares/authMiddleware"); // Updated import
-
-const router = express.Router();
-
-// Protected profile update endpoints
-router.put("/me", verifyToken, updateProfile);
-router.put("/me/password", verifyToken, updatePassword);
-
-module.exports = router;
