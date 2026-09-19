b390cde6b0429412ab8f6e75f084474bc40bf53e
Gaurav775-Git <gaurav775.official@gmail.com>
2026-09-19 10:28:00 +0530

    Cookie age extended because of short time period of the cookie .

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -15,7 +15,7 @@ const createTokenAndSetCookie = (res, user) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    maxAge: 15 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
   return token;
